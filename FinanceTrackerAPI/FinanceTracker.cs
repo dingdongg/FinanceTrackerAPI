e302f2a6b07db87473619b55747d69739466cfa0
donggyu <donggyu258@gmail.com>
2022-05-08 11:29:46 -0700

    Add project files.

---
+++ FinanceTrackerAPI/FinanceTracker.cs
@@ -0,0 +1,11 @@
+﻿namespace FinanceTrackerAPI
+{
+    public class FinanceTracker
+    {
+        public int Id { get; set; }
+        public string TransactionName { get; set; } = String.Empty;
+        public float Amount { get; set; }
+        public DateOnly TransactionDate { get; set; }
+        public string Description { get; set; } = String.Empty;
+    }
+}
