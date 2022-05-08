e707bdbccf9e530ab4f6e1130e1ee44dee9b8014
donggyu <donggyu258@gmail.com>
2022-05-08 13:48:18 -0700

    builder now connects to the database using the DB context

---
+++ FinanceTrackerAPI/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FinanceTrackerAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<FinanceTracker> FinanceTrackers { get; set; }
+    }
+}
