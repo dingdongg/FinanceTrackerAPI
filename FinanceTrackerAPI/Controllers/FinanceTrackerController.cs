e79c6ca9916c684762d4b9d6e121d964c13676b2
donggyu <donggyu258@gmail.com>
2022-05-08 11:41:15 -0700

    created the HTTP GET request method in FinanceTrackerController

---
+++ FinanceTrackerAPI/Controllers/FinanceTrackerController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceTrackerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FinanceTrackerController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var transactions = new List<FinanceTracker>
+            {
+                new FinanceTracker
+                {
+                    Id = 1,
+                    TransactionName = "dummy transaction",
+                    Amount = 250.99,
+                    TransactionDate = new DateOnly(1, 1, 2001),
+                    Description = "i bought something incredibly expensive"
+                }
+            };
+
+            return Ok(transactions);
+        }
+    }
+}
--- FinanceTrackerAPI/FinanceTracker.cs
+++ FinanceTrackerAPI/FinanceTracker.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string TransactionName { get; set; } = String.Empty;
-        public float Amount { get; set; }
+        public double Amount { get; set; }
         public DateOnly TransactionDate { get; set; }
         public string Description { get; set; } = String.Empty;
     }
