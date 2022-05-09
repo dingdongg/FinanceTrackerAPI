e95cfaba9184428a685a309948a71e0dff0b6ed1
donggyu <donggyu258@gmail.com>
2022-05-08 19:16:13 -0700

    created data transfer objects for HTTP READ responses

---
+++ FinanceTrackerAPI/DTOs/TransactionReadDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinanceTrackerAPI.DTOs
+{
+    public class TransactionReadDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = String.Empty;
+
+        public double Amount { get; set; }
+
+        public int Year { get; set; }
+
+        public int Month { get; set; }
+
+        public int Day { get; set; }
+    }
+}
