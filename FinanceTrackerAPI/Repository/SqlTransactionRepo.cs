b61e0b9cedc61226f62e53f6e72985463d596cd8
donggyu <donggyu258@gmail.com>
2022-05-08 19:00:41 -0700

    created implementation scaffolding and injected dependency on DataContext

--- FinanceTrackerAPI/Repository/SqlTransactionRepo.cs
+++ FinanceTrackerAPI/Repository/SqlTransactionRepo.cs
@@ -1,6 +1,41 @@
 ﻿namespace FinanceTrackerAPI.Repository
 {
-    public class SqlTransactionRepo
+    public class SqlTransactionRepo : ITransactionRepo
     {
+        private readonly DataContext _context;
+
+        public SqlTransactionRepo(DataContext context)
+        {
+            _context = context;
+        }
+        public void CreateTransaction(Transaction trctn)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteTransaction(Transaction trctn)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Transaction> GetAllTransactions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Transaction GetTransactionById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateTransaction(Transaction trctn)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
